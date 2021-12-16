da0e631e3c97038722fd429fb9dc264396bda9cd
leemamas <76854100@163.com>
2021-12-16 19:22:56 +0800

    v0.2 增加收藏列表,优化选择

--- main.py
+++ main.py
@@ -6,23 +6,131 @@ import os
 
 
 class Console(cmd.Cmd):
-    intro = 'Music World!'
+    intro = '''
+            ⭐Music World⭐
+    ---------------------------- 
+    相关命令列表       
+    search           搜索音乐
+    love             喜爱音乐
+    exit             退出'''
     prompt = 'music>'
 
-    def do_search(self, arg):
+    def do_s(self,arg):
+        self.do_search(arg)
 
+    def do_search(self, arg):
+        print('Search the music you want')
         key = input('song:')
-        tb, self.playlist = display(craw_music(key))
-        print(tb)
+        json = craw_music(key)
+        playlist = info(json)
+        # print(playlist)
+        if playlist:
+            console(playlist)
+
+            while 1:
+                key = input('choice (1/play 2/collect 3/quit) :')
+
+                def play():
+                    self.play(playlist)
+
+                def collect():
+                    try:
+                        key = input('Enter the ID to collect:')
+                        id = int(key) - 1
+                        title = playlist[id]['title']
+                        author = playlist[id]['author']
+                        url = playlist[id]['url']
+                        f = open("love.txt", 'a')
+                        f.write(title + '|' + author + '|' + url + '\n')
+                        print('Collect song【' + title + '】 success!')
+                        f.close()
+                    except:
+                        pass
+
+                def quit():
+                    print('quit....')
+                    pass
+
+                switch = {
+                    '1': play,
+                    '2': collect,
+                    '3': quit
+                }
+                try:
+                    switch[key]()
+                    break
+                except KeyError as e:
+                    print('Error instruction!')
+        else:
+            print('No relevant information was found!')
 
     def do_love(self, arg):
-        print('my love!')
+        print('My favorite playlist')
+        playlist = []
+        try:
+            f = open("love.txt", "r")
+            for line in f.readlines():
+                line = line.strip()
+                value = line.split('|')
+                key = ['title', 'author', 'url']
+                playlist.append(dict(zip(key, value)))
+            if playlist:
+                console(playlist)
 
-    def do_play(self, arg):
+                while 1:
+                    key = input('choice (1/single play 2/all play 3/edit list 4/quit) :')
+
+                    def singlePlay():
+                        self.play(playlist)
+
+                    def allPlay():
+                        print('developing...')
+
+                    def edit():
+                        delkey=input('Want to delete the song ID:')
+                        try:
+                            delid = int(delkey)
+                        except:
+                            print('please id number')
+
+                        with open("love.txt", "r") as f1:
+                            lines = f1.readlines()
+
+                        with open("love.txt", "w") as f2_w:
+                            count = 1
+                            for line in lines:
+
+                                if count != delid:
+                                    f2_w.write(line)
+                                count += 1
+                        f1.close()
+                        f2_w.close()
+                    def quit():
+                        print('quit....')
+                        pass
+
+                    switch = {
+                        '1': singlePlay,
+                        '2':allPlay,
+                        '3':edit,
+                        '4': quit
+                    }
+                    try:
+                        switch[key]()
+                        break
+                    except KeyError as e:
+                        print('Error instruction!')
+            else:
+                print('No playlist!')
+
+        except:
+            print('No playlist!')
+
+    def play(self, playlist):
         key = input('Enter the ID to play:')
         id = int(key) - 1
         try:
-            cmd = 'mpg123\mpg123.exe -q ' + self.playlist[id] + ''
+            cmd = 'mpg123\mpg123.exe -q ' + playlist[id]['url'] + ''
             print('playing....')
             try:
                 os.system(cmd)
@@ -37,7 +145,6 @@ class Console(cmd.Cmd):
         exit()
 
 
-
 def craw_music(title, type='netease', page=1):
     url = 'http://www.xmsj.org/'
     headers = {
@@ -49,26 +156,32 @@ def craw_music(title, type='netease', page=1):
     }
     params = {'input': title, 'filter': 'name', 'type': type, 'page': page}
     res = rq.post(url, params, headers=headers)
-    info = res.json()
-    return info
+    json = res.json()
+    return json
 
 
-def display(info):
-    tb = PrettyTable()
-    tb.field_names = ['序号', '歌曲', '歌手', 'link', 'url']
-    lt = []
+def info(json):
+    playlist = []
     for i in range(0, 10):
         try:
-            title = jsonpath(info, '$..title')[i]
-            author = jsonpath(info, '$..author')[i]
-            link = jsonpath(info, '$..link')[i]
-            url1 = jsonpath(info, '$..url')[i]
-            lt.append(url1)
-            pic = jsonpath(info, '$..pic')[i]
-            tb.add_row([i + 1, title, author, link, url1])
+            title = jsonpath(json, '$..title')[i]
+            author = jsonpath(json, '$..author')[i]
+            link = jsonpath(json, '$..link')[i]
+            url1 = jsonpath(json, '$..url')[i]
+            playlist.append({'title': title, 'author': author, 'url': url1})
+            pic = jsonpath(json, '$..pic')[i]
+
         except:
             pass
-    return tb, lt
+    return playlist
+
+
+def console(playlist):
+    tb = PrettyTable()
+    tb.field_names = ['序号', '歌曲', '歌手', 'url']
+    for i in range(0, len(playlist)):
+        tb.add_row([i + 1, playlist[i]['title'], playlist[i]['author'], playlist[i]['url']])
+    print(tb)
 
 
 if __name__ == '__main__':
